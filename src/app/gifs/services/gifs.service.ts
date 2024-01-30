da30ccc09457b265cedce268c5ca9807fdd3b838
Josiel Castillo <josielcastillov@gmail.com>
2024-01-30 17:29:34 -0600

    Capitulo 96

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+//const GIPHY_API_KEY = 'HgkKiJ0FhP5Ij8ZZ3xORknWFNUEq6g2o';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'HgkKiJ0FhP5Ij8ZZ3xORknWFNUEq6g2o';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-
-  constructor() { }
+  constructor(private http: HttpClient ) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory]; //Retorna una copia del array this._tagsHistory
@@ -22,7 +29,9 @@ export class GifsService {
 
     }
 
-    this._tagsHistory.unshift
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+
 
   }
 
@@ -31,10 +40,16 @@ export class GifsService {
     if( tag.length === 0) return;
     this.organizeHistory(tag);
 
-    this._tagsHistory.unshift(tag);
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
 
+    this.http.get<SearchResponse>(` ${ this.serviceUrl }/search?`,{params})
+      .subscribe(resp => {
+
+        this.gifList = resp.data;
+        console.log(resp);
+      });
   }
-
-
-
 }
